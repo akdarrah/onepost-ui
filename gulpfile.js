b9aaa80e8b871a6c7f1a132d97c70ec3e4272469
Adam Darrah <piremies@gmail.com>
2021-02-26 12:20:07 -0500

    Trying to build svelte app with gulp and browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,25 @@
 const {dest, src, task} = require('gulp');
+const browserify = require('browserify');
+var babelify = require("babelify");
+const source = require('vinyl-source-stream');
+const buffer = require('vinyl-buffer');
 const gulpSvelte = require('gulp-svelte');
 
 task('default', () => {
-  return src('src/OnepostUI.js')
-    .pipe(gulpSvelte())
+  return browserify({
+      basedir: '.',
+      debug: true,
+      entries: ['./src/OnepostUI.js'],
+      cache: {},
+      packageCache: {}
+    })
+    .transform(babelify.configure({
+      presets: ["@babel/preset-env"]
+    }))
+    .bundle().on('error', (e) => console.log(e))
+    .pipe(source('OnepostUI.js'))
+    .pipe(buffer())
     .pipe(dest('dist'));
+
+    // .pipe(gulpSvelte())
 });
