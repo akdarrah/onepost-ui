b033e50196e79976516abb431bc1531de88e4293
Adam Darrah <piremies@gmail.com>
2021-03-01 16:33:56 -0500

    We need to know the target element

--- spec/javascript/OnepostUI.spec.ts
+++ spec/javascript/OnepostUI.spec.ts
@@ -2,8 +2,10 @@ import { OnepostUI } from '../../src/OnepostUI';
 
 describe('OnepostUI#constructor', () => {
   it('can be initialized', () => {
-    let onepost = new OnepostUI("pk-12345", [1,2,3]);
+    let element = document.body;
+    let onepost = new OnepostUI(element, "pk-12345", [1,2,3]);
 
+    expect(onepost.target).toBe(element);
     expect(onepost.publicKey).toBe("pk-12345");
     expect(onepost.authorizedPageIds).toEqual([1,2,3]);
   })
--- src/OnepostUI.ts
+++ src/OnepostUI.ts
@@ -1,8 +1,12 @@
 export class OnepostUI {
+  readonly endpoint = "https://api.getonepost.com/post_intents/new";
+
+  public target: HTMLElement;
   public publicKey: string;
   public authorizedPageIds: Array<number>;
 
-  constructor(publicKey: string, authorizedPageIds: Array<number>) {
+  constructor(target: HTMLElement, publicKey: string, authorizedPageIds: Array<number>) {
+    this.target = target;
     this.publicKey = publicKey;
     this.authorizedPageIds = authorizedPageIds;
   }
